d53171e3aa444681dc0faa1b017f5f8092694a8a
Supriyo <spdh427@gmail.com>
2025-04-03 02:26:32 +0530

    a

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: 'http://localhost:517'
+    // origin: 'http://localhost:5173'
     origin: 'https://movie-card-app-orpin.vercel.app'
   });
 
