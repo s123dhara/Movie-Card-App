ba996a82cffca4eff1fc0afdacab1a923646bd41
Supriyo <spdh427@gmail.com>
2025-04-02 15:31:38 +0530

    "fix in api.js"

--- src/services/api.js
+++ src/services/api.js
@@ -44,9 +44,12 @@ export const searchMovies = async (query) => {
       )}`,
       options
     );
-    const data = await response.json();
-    // console.log(data);
-    // return [];
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
+    const data = await response.json();    
     return data;
   } catch (error) {
     console.error('Error fetching data:', error);
