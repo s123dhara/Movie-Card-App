a85f12968df69bfe980915e9a13ed3278489be96
Supriyo <spdh427@gmail.com>
2025-04-03 02:14:23 +0530

    UPDATE ALL CODE

--- backend/src/bk_main.ts
+++ backend/src/bk_main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 
-
+// dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: 'http://localhost:5173'
-    origin: 'https://movie-card-app-orpin.vercel.app'
+    origin: 'http://localhost:5173'
   });
 
   const configService = app.get(ConfigService);
